efef3c2e7b95e1121b0590650d2aa0c56b1ae276
Samuel Obuch <456163@mail.muni.cz>
2020-03-17 18:47:55 +0100

    TennisGame2: use correct variables, remove unused functionality

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
+        private int p1point = 0;
+        private int p2point = 0;
 
         private string p1res = "";
         private string p2res = "";
@@ -13,7 +13,6 @@ namespace Tennis
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
             this.player2Name = player2Name;
         }
 
@@ -85,39 +84,23 @@ namespace Tennis
 
             if (p1point > p2point && p2point >= 3)
             {
-                score = "Advantage player1";
+                score = "Advantage " + player1Name;
             }
 
             if (p2point > p1point && p1point >= 3)
             {
-                score = "Advantage player2";
+                score = "Advantage " + player2Name;
             }
 
             if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
             {
-                score = "Win for player1";
+                score = "Win for " + player1Name;
             }
             if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
             {
-                score = "Win for player2";
+                score = "Win for " + player2Name;
             }
             return score;
-        }
-
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
         }
 
         private void P1Score()
@@ -132,12 +115,11 @@ namespace Tennis
 
         public void WonPoint(string player)
         {
-            if (player == "player1")
+            if (player == player1Name)
                 P1Score();
-            else
+            if (player == player2Name)
                 P2Score();
         }
-
     }
 }
 
