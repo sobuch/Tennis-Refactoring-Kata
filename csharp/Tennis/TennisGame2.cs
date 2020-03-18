c3289f63e7e8e48a6ec5a1f785f4b34f1c739beb
Samuel Obuch <456163@mail.muni.cz>
2020-03-18 21:49:39 +0100

    TennisGame2: More early returns + reorder not to break the logic

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -18,6 +18,7 @@ namespace Tennis
             string p1res = "";
             string p2res = "";
             string score = "";
+
             if (p1point == p2point && p1point < 3)
             {
                 if (p1point == 0)
@@ -28,8 +29,27 @@ namespace Tennis
                     score = "Thirty";
                 return score + "-All";
             }
+
             if (p1point == p2point)
                 return "Deuce";
+
+            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
+            {
+                return "Win for " + player1Name;
+            }
+            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
+            {
+                return "Win for " + player2Name;
+            }
+
+            if (p1point > p2point && p2point >= 3)
+            {
+                return "Advantage " + player1Name;
+            }
+            if (p2point > p1point && p1point >= 3)
+            {
+                return "Advantage " + player2Name;
+            }
 
             if (p1point > 0 && p2point == 0)
             {
@@ -79,25 +99,6 @@ namespace Tennis
                 if (p1point == 2)
                     p1res = "Thirty";
                 score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p2point >= 3)
-            {
-                score = "Advantage " + player1Name;
-            }
-
-            if (p2point > p1point && p1point >= 3)
-            {
-                score = "Advantage " + player2Name;
-            }
-
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for " + player1Name;
-            }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for " + player2Name;
             }
             return score;
         }
