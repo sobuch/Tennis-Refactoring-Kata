98e252260dfb62e537fa533af08f2e23eafb251e
Samuel Obuch <456163@mail.muni.cz>
2020-03-18 21:53:53 +0100

    TennisGame2: Simplify conditions

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -51,56 +51,25 @@ namespace Tennis
                 return "Advantage " + player2Name;
             }
 
-            if (p1point > 0 && p2point == 0)
-            {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > 0 && p1point == 0)
-            {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
+            if (p1point == 0)
                 p1res = "Love";
-                score = p1res + "-" + p2res;
-            }
+            if (p1point == 1)
+                p1res = "Fifteen";
+            if (p1point == 2)
+                p1res = "Thirty";
+            if (p1point == 3)
+                p1res = "Forty";
 
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > p1point && p2point < 4)
-            {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            return score;
+            if (p2point == 0)
+                p2res = "Love";
+            if (p2point == 1)
+                p2res = "Fifteen";
+            if (p2point == 2)
+                p2res = "Thirty";
+            if (p2point == 3)
+                p2res = "Forty";
+
+            return score = p1res + "-" + p2res;
         }
 
         private void P1Score()
